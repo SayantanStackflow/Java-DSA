8cc9dadf09b382e737ff69ed751f638524e5b5a1
SayantanStackflow <sayantan.college6291@gmail.com>
2022-10-10 09:41:57 +0100

    increment and decrement operator

---
+++ operators.java
@@ -0,0 +1,12 @@
+public class operators {
+    public static void main(String[] args) {
+        int a= 4;
+        int b= 8;
+        int c= 10;
+        System.out.println(++b *9);
+        System.out.println(b);
+        char ch= 'p';
+        System.out.println(ch++);
+        System.out.println(ch);
+    }
+}
